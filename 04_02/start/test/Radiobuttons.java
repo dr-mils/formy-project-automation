dc110c12a02dff9c429015b10bdce35a8671978a
Milosz Wojciechowski <miloszwojciechowski@MacBook-Air-Miosz.local>
2022-09-08 15:51:49 +0200

    Initial commt

---
+++ 04_02/start/test/Radiobuttons.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Radiobuttons {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/miloszwojciechowski/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+
+        WebElement checkBox1 = driver.findElement(By.id("checkbox-1"));
+        checkBox1.click();
+
+        WebElement checkBox2 = driver.findElement(By.id("checkbox-2"));
+        checkBox2.click();
+
+        WebElement checkBox3 = driver.findElement(By.id("checkbox-3"));
+        checkBox3.click();
+
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/miloszwojciechowski/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("John");
+        driver.findElement(By.id("last-name")).sendKeys("Doe");
+        driver.findElement(By.id("job-title")).sendKeys("CEO");
+
+        driver.findElement(By.id("radio-button-2")).click();
+
+        driver.findElement(By.id("checkbox-2")).click();
+
+        driver.findElement(By.cssSelector("option[value='1']")).click();
+
+        driver.findElement(By.id("datepicker")).sendKeys("01/01/2023");
+        Thread.sleep(1000);
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        
+        driver.quit();
+    }
+}
